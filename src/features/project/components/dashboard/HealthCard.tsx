7eac9b7b4860ce90b6960430967a7f3acb9a3703
nikola04 <nikolanedeljkovic.official@gmail.com>
2025-08-12 13:08:27 +0000

    HealthCard: Fix health messages display and loading state handling

--- src/features/project/components/dashboard/HealthCard.tsx
+++ src/features/project/components/dashboard/HealthCard.tsx
@@ -1,15 +1,24 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { CircleProgress } from "@/components/ui/CircleProgress";
+import { Skeleton } from "@/components/ui/skeleton";
 import type { IHealth } from "@/types/project";
 import { TriangleAlert } from "lucide-react";
 import { useMemo } from "react";
+
+type Warnings = {
+    title: string,
+    messages: {
+        name: string,
+        state: string
+    }[]
+}
 
 export default function HealthCard({ health, loading }: {
     health: IHealth|null;
     loading: boolean;
 }) {
     if(loading)
-        return null;
+        return <HealthCardSkeleton />;
 
     if(!health) 
         return null;
@@ -19,7 +28,7 @@ export default function HealthCard({ health, loading }: {
     const statusColor = useMemo(() => {
         if(health.status === "critical") return "var(--danger)";
         if(health.status === "degraded") return "var(--warning)";
-        return "var(--success)"
+        return "var(--primary)"
     }, [health.status])
 
     const warnings: Warnings[] = useMemo(() => [{
@@ -31,12 +40,12 @@ export default function HealthCard({ health, loading }: {
     }], [health]);
 
     return <Card className="">
-        <CardContent className="flex gap-8 px-6">
-            <div className="flex flex-col justify-center gap-4">
+        <CardContent className="flex flex-col md:flex-row gap-8 px-6">
+            <div className="flex flex-col justify-center items-center gap-4">
                 <CircleProgress 
                     strokeWidth={14}
                     bgColor="var(--background-alt)"
-                    color="var(--primary)"
+                    color={statusColor}
                     progress={health.overallHealth}
                     className="flex items-center justify-center" >
                     <p className="font-semibold">{healthString}</p>
@@ -46,28 +55,35 @@ export default function HealthCard({ health, loading }: {
                     <p className="text-xs text-center text-muted-foreground">overall score</p>
                 </div>
             </div>
-            <div className="flex flex-col">
-                <WarnMessages warnings={warnings} />
-            </div>
+            <WarnMessages warnings={warnings} />
         </CardContent>
     </Card>
 }
 
-type Warnings = {
-    title: string,
-    messages: {
-        name: string,
-        state: string
-    }[]
+function HealthCardSkeleton(){
+    return <Card className="">
+        <CardContent className="flex flex-col md:flex-row gap-8 px-6">
+            <div className="flex flex-col justify-center items-center gap-4">
+                <Skeleton className="w-[100px] h-[100px] overflow-hidden rounded-full"/>
+                <div className="flex flex-col items-center gap-2 justify-center pt-1">
+                    <Skeleton className="w-16 h-3" />
+                    <Skeleton className="w-20 h-2" />
+                </div>
+            </div>
+            <div className="flex flex-col py-2">
+                <Skeleton className="w-32 h-3" />
+            </div>
+        </CardContent>
+    </Card>
 }
 
 function WarnMessages({ warnings }: {
     warnings: Warnings[]
 }){
     if(warnings.length === 0 || warnings.every(warns => warns.messages.length === 0))
-        return <p className="text-sm text-muted-foreground">No warnings.</p>;
+        return <p className="py-2 text-sm text-muted-foreground">No warnings.</p>;
 
-    return <div className="flex gap-4">
+    return <div className="flex flex-col md:flex-row gap-8 py-2">
         { warnings.map((warns, ind) => <WarnMessage key={ind} title={warns.title} messages={warns.messages} />) }
     </div>
 }
@@ -87,14 +103,14 @@ function WarnMessage({ title, messages }: {
     
     if(oversize) messagesPreview.length = 2;
 
-    return <div className="flex flex-col gap-2 p-2">
+    return <div className="flex flex-col items-center md:items-start gap-2">
         <div className="flex gap-1 items-center">
             <TriangleAlert size={14} color="var(--warning)" />
             <p className="text-sm" style={{ color: 'var(--warning)'}}>{ title }</p>
         </div>
         <div>
             { messagesPreview.map((message, ind) => <div key={ind}>
-                <p className="flex items-center gap-2 text-muted-foreground text-sm">
+                <p className="text-muted-foreground text-sm text-center md:text-left">
                     <span>{ message.name } is { message.state }!</span>
                 </p>
             </div>)}
